8ad0fc2e1e65a6553745e4ceb41b9c1ba2a0cbfd
CooperWolf <cooperwolf98@gmail.com>
2019-07-26 18:03:34 -0500

    Working on game tick

--- Snake.cpp
+++ Snake.cpp
@@ -2,23 +2,38 @@
 //
 
 #include <iostream>
+#include <thread>
+#include <chrono>
 
 using namespace std;
 
-void tick();
+void tick(char board[5][20]);
 
 int main()
 {
-	tick();
+	bool running = true;
+	while (running)
+	{
+		// speed is the parameter which controls how often the game is updated. Lower number is faster
+		// wait to update state based on speed
+		this_thread::sleep_for(chrono::milliseconds(100));
+		char game[5][20];
+
+		tick(game);
+	}
 }
 
-void tick() 
+void tick(char board[5][20]) 
 {
-	for (int i = 0; i < 20; i++) 
-	{
-		// clear console
-		system("CLS");
-		// draw new board
-		cout << i;
+	// clear console
+	system("CLS");
+	// draw new board
+	string gameBoard = "";
+	for (int i = 0; i < 5; i++) {
+		for (int j = 0; j < 20; j++) {
+			// append each row and print out all at once
+			gameBoard = gameBoard + board[i][j];
+		}
 	}
+	cout << gameBoard;
 }
